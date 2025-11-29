b52bbde65dafdd576bdf4a28d3224914fe8f07b1
Welinton Gomes Batista <133980158+welinton19@users.noreply.github.com>
2025-11-29 16:22:37 -0300

    implementando ao program jtw

--- JogoJusto/Program.cs
+++ JogoJusto/Program.cs
@@ -6,46 +6,22 @@ using JogoJusto.Middleware;
 using JogoJusto.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+//#region Tratamento de erro global
+//builder.Services.AddTransient<ErrorHanddlerMiddleware>();
+//#endregion
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
 builder.Services.AddScoped<IAuthorizationHandler, RoleAuthorizationHandler>();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e9f058c72cc66c7740d97fda56ac8c157e6a6c2c
-#region service e repositorio
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-builder.Services.AddScoped<IDepartamentoRepository, DepartamentoRepository>();
-builder.Services.AddScoped<IDesenvolvimentoRepository, DesenvolvimentoRepository>();
-builder.Services.AddScoped<IEmpresaRepository, EmpresaRepository>();
-builder.Services.AddScoped<IEsgLogRepository, EsgLogRepository>();
-builder.Services.AddScoped<IFuncionarioRepository, FuncionarioRepository>();
-builder.Services.AddScoped<IMetaEsgRepository, MetaEsgRepository>();
-builder.Services.AddScoped<ITokenRepository, TokenRepository>();
-
-
-builder.Services.AddScoped<IDepartamentoService, DepartamentoService>();
-builder.Services.AddScoped<IDesenvolvimentoService, DesenvolvimentoService>();
-builder.Services.AddScoped<IEmpresaService, EmpresaService>();
-builder.Services.AddScoped<IEsgLogService, EsgLogService>();
-builder.Services.AddScoped<IFuncionarioService, FuncionarioService>();
-builder.Services.AddScoped<IMetaEsgService, MetaEsgService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IUsuarioService, UsuarioService>();
-
-#endregion
-
-<<<<<<< HEAD
-#region configigurando autorizacao de papel
-
-builder.Services.AddScoped<TokenService, TokenService>();
-builder.Services.AddScoped<IUsuarioService, UsuarioService>();
-=======
 builder.Services.AddScoped<IEmpresaRepository, EmpresaRepository>();
 builder.Services.AddScoped<IEmpresaService, EmpresaService>();
 
@@ -65,12 +41,7 @@ builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
->>>>>>> ad8a2cb6833c033c5941ef528eb45982475261d2
 
-=======
-#region configurando autorizacao de papel
-
->>>>>>> e9f058c72cc66c7740d97fda56ac8c157e6a6c2c
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
